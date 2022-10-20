56278986965c1f5ce8ca350df807a4c1050170dc
Fer <afriguez@gmail.com>
2022-10-20 11:42:05 -0500

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-function App() {
+import { Sidebar } from "./components/Sidebar";
+
+export const App = () => {
   return (
-    <div className="App">
+    <div className="App m-5">
+      <Sidebar />
     </div>
   );
-}
-
-export default App;
+};
