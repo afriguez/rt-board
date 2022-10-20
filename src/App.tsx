bb3300e7f096c4578b57cf7072214634d4542811
Fer <afriguez@gmail.com>
2022-10-20 13:13:21 -0500

    feat: add height & padding to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from "./components/Sidebar";
 
 export const App = () => {
   return (
-    <div className="App m-5">
+    <div className="App h-screen w-full flex p-5 items-center">
       <Sidebar />
     </div>
   );
