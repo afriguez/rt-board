399e7e915e382e2de1b031e4f5907b8c2d230297
Fer <afriguez@gmail.com>
2022-10-20 13:12:45 -0500

    feat: update sidebar size

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,11 @@ import { ArrowIcon, PencilIcon, ShapeIcon, TextIcon } from "./Icons";
 
 export const Sidebar = () => {
   return (
-    <div className="flex h-[314px] w-[78px] flex-col items-center justify-between rounded-lg bg-white shadow-md">
+    <div className="flex h-[250px] w-[50px] flex-col items-center justify-between rounded-lg bg-white shadow-md">
       <IconBtn children={<PencilIcon />} />
       <IconBtn children={<ArrowIcon />} />
       <IconBtn children={<TextIcon />} />
-      <div className="my-1 h-3 w-[64px] rounded bg-black"></div>
+      <div className="my-1 h-2 w-[80%] rounded bg-black"></div>
       <IconBtn children={<ShapeIcon />} />
     </div>
   );
